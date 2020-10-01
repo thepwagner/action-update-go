343f4dd2e6f8da4410a34b40e5e52b8fa24e5202
Peter Wagner <git@mycloudand.me>
2020-09-30 20:03:42 -0400

    pullrequest: signed descriptor

--- repo/pullrequest.go
+++ repo/pullrequest.go
@@ -2,10 +2,14 @@ package repo
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/sha512"
+	"crypto/subtle"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"sort"
 	"strings"
 
 	"github.com/dependabot/gomodules-extracted/cmd/go/_internal_/semver"
@@ -115,4 +119,42 @@ func writePatchBlob(out io.Writer, updates ...updater.Update) {
 	_, _ = fmt.Fprintln(out, "\n```json")
 	_ = encoder.Encode(&details)
 	_, _ = fmt.Fprint(out, "```\n")
+}
+
+type SignedUpdateDescriptor struct {
+	Updates   []updater.Update `json:"updates"`
+	Signature []byte           `json:"signature"`
+}
+
+func NewSignedUpdateDescriptor(key []byte, updates ...updater.Update) (SignedUpdateDescriptor, error) {
+	signature, err := updatesHash(key, updates)
+	if err != nil {
+		return SignedUpdateDescriptor{}, err
+	}
+	return SignedUpdateDescriptor{
+		Updates:   updates,
+		Signature: signature,
+	}, nil
+}
+
+func updatesHash(key []byte, updates []updater.Update) ([]byte, error) {
+	sort.Slice(updates, func(i, j int) bool {
+		return updates[i].Path < updates[j].Path
+	})
+	hash := hmac.New(sha512.New, key)
+	if err := json.NewEncoder(hash).Encode(updates); err != nil {
+		return nil, err
+	}
+	return hash.Sum(nil), nil
+}
+
+func VerifySignedUpdateDescriptor(key []byte, descriptor SignedUpdateDescriptor) ([]updater.Update, error) {
+	calculated, err := updatesHash(key, descriptor.Updates)
+	if err != nil {
+		return nil, fmt.Errorf("calculating signature: %w", err)
+	}
+	if subtle.ConstantTimeCompare(calculated, descriptor.Signature) != 1 {
+		return nil, fmt.Errorf("invalid signature")
+	}
+	return descriptor.Updates, nil
 }
--- repo/pullrequest_test.go
+++ repo/pullrequest_test.go
@@ -2,6 +2,9 @@ package repo_test
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -90,4 +93,49 @@ Here are some updates, I hope they work.
   "patch": false
 }
 `+"```"), strings.TrimSpace(body))
+}
+
+func TestNewSignedUpdateDescriptor(t *testing.T) {
+	key := []byte{1, 2, 3, 4}
+
+	cases := []struct {
+		signature string
+		updates   []updater.Update
+	}{
+		{
+			signature: "HAF6zSdBBOsbrLRClce7M73tN7VhCdPB6YYhECL/ifDC6DHR0YSGXoY6JQeEaFoncJbxp/afBpY+GVE5DUfWwQ==",
+			updates:   []updater.Update{awsSdkGo13417},
+		},
+		{
+			signature: "kq9CbO3rYkThJPiJgVTfhkfAG4q5aEeXuta0x3wPVdUnqQhitA/FasfJ2WftpfiZvueCnknoX04yxTM94BUn4A==",
+			updates:   []updater.Update{fooBar987},
+		},
+		{
+			signature: "TL6d3v5DKRu8uDY5doooDLLd7mJDHx6U5P4jRZYanLT4VI1dzt1gIRvZGW3G0ZlDQqmuTOftovTlwLHO1VW4Xw==",
+			updates:   []updater.Update{awsSdkGo13417, fooBar987},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("%v", tc.updates), func(t *testing.T) {
+			descriptor, err := repo.NewSignedUpdateDescriptor(key, tc.updates...)
+			require.NoError(t, err)
+
+			buf, err := json.Marshal(&descriptor)
+			require.NoError(t, err)
+			t.Log(string(buf))
+
+			assert.Equal(t, tc.updates, descriptor.Updates)
+			assert.Equal(t, tc.signature, base64.StdEncoding.EncodeToString(descriptor.Signature))
+
+			verified, err := repo.VerifySignedUpdateDescriptor(key, descriptor)
+			require.NoError(t, err)
+			assert.Equal(t, tc.updates, verified)
+		})
+	}
+}
+
+func TestVerifySignedUpdateDescriptor_Invalid(t *testing.T) {
+	_, err := repo.VerifySignedUpdateDescriptor([]byte{}, repo.SignedUpdateDescriptor{})
+	assert.EqualError(t, err, "invalid signature")
 }
